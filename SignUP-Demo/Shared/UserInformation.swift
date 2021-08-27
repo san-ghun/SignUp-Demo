31141f7a0b781896a979a9702bee3293eab66565
Sanghun <po4865+github_sanghun@gmail.com>
2021-08-27 16:24:29 +0900

    added comment in UserInformation.swift to make clear to code with proper segmentation

--- SignUP-Demo/Shared/UserInformation.swift
+++ SignUP-Demo/Shared/UserInformation.swift
@@ -8,16 +8,26 @@
 import Foundation
 
 class UserInformation {
+    
+    // MARK: - Properties
+    // MARK: Set Singletone
     static let shared: UserInformation = UserInformation()
     
+    // MARK:
     var username: String?
     var password: String?
     
+    // MARK:
+    var phoneNumber: String?
+    var dateOfBirth: String?
+    
+    // MARK:
     var profileImage: Data?
     var description: String?
     
-    var phoneNumber: String?
-    var dateOfBirth: String?
+    
+    // MARK: - Methods
+    
     
     
 }
