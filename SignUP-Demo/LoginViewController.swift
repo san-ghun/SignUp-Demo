c8c26cc9d7c2fcd5ed2e76eccefd2bf7c0851dce
Sanghun <po4865+github_sanghun@gmail.com>
2021-08-27 16:29:45 +0900

    Starting point to make view with the interface builder - added IBOutlets in LoginView

--- SignUP-Demo/LoginViewController.swift
+++ SignUP-Demo/LoginViewController.swift
@@ -8,11 +8,31 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    
+    // MARK: - Properties
+    
+    // MARK: IBOutlets
+    @IBOutlet weak var titleImageView: UIImageView!
+    @IBOutlet weak var idTextField: UITextField!
+    @IBOutlet weak var pwdTextField: UITextField!
+    @IBOutlet weak var signInButton: UIButton!
+    @IBOutlet weak var signUpButton: UIButton!
+    
+    // MARK: Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
+    
+    // MARK: - Methods
+    
+    // MARK: Custom Methods
+    
+    // MARK: View with code
+    
+    // MARK: IBActions
 
 
 }
