e08f03fa182621305cff07da83e4d3f94ded380e
Sanghun <po4865+github_sanghun@gmail.com>
2021-09-08 17:03:53 +0900

    added lines of code in LoggedInViewController - added lines of code as a simple working view

--- SignUP-Demo/LoggedInViewController.swift
+++ SignUP-Demo/LoggedInViewController.swift
@@ -9,10 +9,22 @@ import UIKit
 
 class LoggedInViewController: UIViewController {
 
+    private let button = UIButton()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .systemGreen
+        title = "Logged In"
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Dismiss", style: .plain, target: self, action: #selector(dismissSelf))
+        
+        button.setTitle("Push controller", for: .normal)
+        view.addSubview(button)
+        button.frame = CGRect(x: 100, y: 100, width: 200, height: 52)
+        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
     }
     
 
@@ -25,5 +37,15 @@ class LoggedInViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    @objc private func didTapButton() {
+        let vc = UIViewController()
+        vc.view.backgroundColor = .white
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    @objc private func dismissSelf() {
+        dismiss(animated: true, completion: nil)
+    }
 
 }
